25eaf500aae5e0af97d20f6d4df2a2cfb9a01f47
Robert Byrne <replit@robert.byrnemail.org>
2019-12-23 23:17:09 +0000

    Not much

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 const App = {
+    methods: {
+        onClickAwesome(ev){
+            alert(`Isn't it!`);
+        }
+    },
     render(){
-        return <div id="app">App goes here</div>;
+        return (
+            <div id="app">
+                App goes here
+                <p>
+                <button onClick={this.onClickAwesome}>Awesome</button>
+                </p>
+            </div>
+        );
     }
 }
 
