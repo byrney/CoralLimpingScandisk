e60e4032d46edb648366e8d075898a3a271bd276
Robert Byrne <robert@byrnemail.org>
2019-12-25 06:55:10 +0000

    Basic injection working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
+import TwoProps from './TwoProps.js';
+import StoreView from './StoreView.js';
+import StoreInject from './StoreInject.js';
+
 const App = {
-    methods: {
-        onClickAwesome(ev){
-            alert(`Isn't it!`);
+
+    data(){
+        return {
+            size: 'massive'
         }
     },
+
+    methods: {
+
+        onColourChange(newVal){
+            this.colour = newVal;
+        }
+    },
+
     render(){
+        const twoProps = (scope) => (
+            <TwoProps
+                colour={scope.colour}
+                size={scope.size}
+                onColour={scope.onColourChange}
+            />
+        );
         return (
             <div id="app">
-                App goes here
-                <p>
-                <button onClick={this.onClickAwesome}>Awesome</button>
-                </p>
+                <section class='app'>
+                    <h2>App</h2>
+                        <StoreInject
+                            size={this.size}
+                            scopedSlots={ {default: twoProps} }
+                        />
+                </section>
+                <StoreView />
             </div>
         );
     }
-}
+
+};
+
 
 export default App;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.js'
+import Vuex from 'vuex'
 import './styles.css'
+
+Vue.use(Vuex);
+
+const theStore = new Vuex.Store({
+       state: {
+           storeColour: 'red'
+       },
+       mutations: {
+           storeColour(state, newv){
+                state.storeColour = newv;
+           }
+       }
+});
 
 Vue.config.productionTip = false
 new Vue({
+    store: theStore,
     render: h => h(App)
 }).$mount("#app");
