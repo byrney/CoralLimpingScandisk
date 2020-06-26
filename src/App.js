deef6b9f124e2a63deffe1684247c5b9d2bcd9c0
Robert Byrne <robert@byrnemail.org>
2020-06-26 23:55:33 +0100

    WIP: List and edit working

--- src/App.js
+++ src/App.js
@@ -1,32 +1,61 @@
 const _ = require('lodash');
 
+function defaultCourier(){
+    return {
+        id: null,
+        name: 'None',
+        startTime: new Date(0, 0, 0, 9),
+        endTime: new Date(0, 0, 0, 17)
+    }
+}
+
 const FleetView = {
     name: 'FleetView',
     data(){
         return {
             couriers: [{
                 id: 'c0',
-                name: 'Badger'
+                name: 'Badger',
+                startTime: new Date(0, 0, 0, 9),
+                endTime: new Date(0, 0, 0, 17)
             }, {
                 id: 'c1',
-                name: 'Toad'
+                name: 'Toad',
+                startTime: new Date(0, 0, 0, 8),
+                endTime: new Date(0, 0, 0, 22)
             }],
-            selectedCourier: -1,
+            selectedCourierIndex: -1,
         };
+    },
+    computed: {
+        selectedCourier(){
+            const index = this.selectedCourierIndex;
+            return index > -1 ? this.couriers[index] : null;
+        }
     },
     methods: {
         onSelectCourier(index){
-            console.log(index);
-            this.selectedCourier = index;
+            this.selectedCourierIndex = index;
+        },
+        onUpdatedCourier(newVal){
+            const index = _.findIndex(this.couriers, {id: newVal.id});
+            this.couriers.splice(index, 1, newVal);
         }
     },
     render(){
         return (
             <div class="fleet">
+                <h2>Fleet</h2>
                 <CourierListView
                     couriers={this.couriers}
-                    selectedCourier={this.selectedCourier}
-                    onSelectCourier={this.onSelectCourier} />
+                    selectedCourierIndex={this.selectedCourierIndex}
+                    onSelectCourier={this.onSelectCourier}
+                />
+                <h2>Courier</h2>
+                <CourierView
+                    courier={this.selectedCourier}
+                    onUpdate={this.onUpdatedCourier}
+                />
             </div>
         );
     }
@@ -35,7 +64,7 @@ const FleetView = {
 const CourierListView = {
     props: {
         couriers: {type: Array, required: true},
-        selectedCourier: {type: Number, required: true}
+        selectedCourierIndex: {type: Number, required: true}
     },
     name: 'CourierListView',
     methods: {
@@ -48,7 +77,7 @@ const CourierListView = {
             <ul class="couriers">
                 {
                     _.map(this.couriers, (courier, index) => (
-                        <li class="courier-list-item" class={ {selected: index === this.selectedCourier} }>
+                        <li class={ {selected: index === this.selectedCourierIndex} }>
                             <label >
                                 <input
                                    class="hidden"
@@ -59,6 +88,7 @@ const CourierListView = {
                                    onChange={this.onChangeSelected}
                                 />
                                 <span>{courier.name}</span>
+                                <span>{courier.startTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</span>
                             </label>
                         </li>
                     ))
@@ -69,7 +99,81 @@ const CourierListView = {
 
 }
 
+const TimeInput = {
+    props: {
+        value: {type: Date, default: undefined}
+    },
+    methods: {
+        stringToTime(strTime){
+            const arr = strTime.split(':').map(function(item) { return parseInt(item) });
+            return new Date(0, 0, 0, arr[0], arr[1]);
+        },
+        timeToString(datetime){
+            return datetime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+        },
+        onInput(ev){
+            this.$emit('input', this.stringToTime(ev.target.value));
+        }
+    },
+    render(){
+        return (
+            <input
+                type="time"
+                value={this.timeToString(this.value)}
+                onInput={this.onInput}
+            />
+        );
+    }
+};
+
 const CourierView = {
+    props: {
+        courier: {type: Object, default: null}
+    },
+    data(){
+        return {
+            editing: this.courier ? _.clone(this.courier) : defaultCourier()
+        };
+    },
+    watch: {
+        courier(){
+            this.reset();
+        }
+    },
+    methods: {
+        reset(){
+            this.editing = this.courier ? _.clone(this.courier) : defaultCourier()
+        },
+        update(){
+            this.$emit('update', this.editing);
+        },
+    },
+    render(){
+        return (
+            <fieldset class="courier" disabled={this.courier === null}>
+                <label>
+                    <span>name:&nbsp;</span>
+                    <input
+                        type="text"
+                        v-model={this.editing.name}
+                    />
+                </label>
+                <br/>
+                <label>
+                    <span>start:&nbsp;</span>
+                    <TimeInput v-model={this.editing.startTime} />
+                </label>
+                <br/>
+                <label>
+                    <span>end:&nbsp;</span>
+                    <TimeInput v-model={this.editing.endTime}/>
+                </label>
+                <input type="button" value="Update" onClick={this.update}>Update</input>
+                <input type="button" value="Cancel" onClick={this.reset}>Cancel</input>
+            </fieldset>
+        );
+    }
+
 }
 
 
@@ -83,7 +187,6 @@ const App = {
     render(){
         return (
             <div id="app">
-                Hello
                 <FleetView />
             </div>
         );
