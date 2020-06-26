3355088a93d3288c19e19fc169c9766cf54d4b9d
Robert Byrne <robert@byrnemail.org>
2020-06-26 21:30:09 +0100

    Initial list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,90 @@
+const _ = require('lodash');
+
+const FleetView = {
+    name: 'FleetView',
+    data(){
+        return {
+            couriers: [{
+                id: 'c0',
+                name: 'Badger'
+            }, {
+                id: 'c1',
+                name: 'Toad'
+            }],
+            selectedCourier: -1,
+        };
+    },
+    methods: {
+        onSelectCourier(index){
+            console.log(index);
+            this.selectedCourier = index;
+        }
+    },
+    render(){
+        return (
+            <div class="fleet">
+                <CourierListView
+                    couriers={this.couriers}
+                    selectedCourier={this.selectedCourier}
+                    onSelectCourier={this.onSelectCourier} />
+            </div>
+        );
+    }
+};
+
+const CourierListView = {
+    props: {
+        couriers: {type: Array, required: true},
+        selectedCourier: {type: Number, required: true}
+    },
+    name: 'CourierListView',
+    methods: {
+        onChangeSelected(ev){
+            this.$emit('selectCourier', Number(ev.target.value));
+        }
+    },
+    render(){
+        return (
+            <ul class="couriers">
+                {
+                    _.map(this.couriers, (courier, index) => (
+                        <li class="courier-list-item" class={ {selected: index === this.selectedCourier} }>
+                            <label >
+                                <input
+                                   class="hidden"
+                                   type="radio"
+                                   name="couriers"
+                                   value={index}
+                                   key={courier.id}
+                                   onChange={this.onChangeSelected}
+                                />
+                                <span>{courier.name}</span>
+                            </label>
+                        </li>
+                    ))
+                }
+            </ul>
+        )
+    }
+
+}
+
+const CourierView = {
+}
+
+
 const App = {
     methods: {
         onClickAwesome(ev){
             alert(`Isn't it!`);
         }
     },
+    name: 'App',
     render(){
         return (
             <div id="app">
-                App goes here
-                <p>
-                <button onClick={this.onClickAwesome}>Awesome</button>
-                </p>
+                Hello
+                <FleetView />
             </div>
         );
     }
