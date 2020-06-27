5349ac5797a11927a521b6bde0b62e93eb12e196
Robert Byrne <robert@byrnemail.org>
2020-06-27 01:41:56 +0100

    Mostly working

--- src/App.js
+++ src/App.js
@@ -9,21 +9,40 @@ function defaultCourier(){
     }
 }
 
+function demoCouriers(){
+    return [{
+        id: _.uniqueId(),
+        name: 'Badger',
+        startTime: new Date(0, 0, 0, 9, 15),
+        endTime: new Date(0, 0, 0, 17)
+    }, {
+        id: _.uniqueId(),
+        name: 'Toad',
+        startTime: new Date(0, 0, 0, 8, 45),
+        endTime: new Date(0, 0, 0, 21)
+    }, {
+        id: _.uniqueId(),
+        name: 'Weasle',
+        startTime: new Date(0, 0, 0, 6, 15),
+        endTime: new Date(0, 0, 0, 16, 30)
+    }, {
+        id: _.uniqueId(),
+        name: 'Mole',
+        startTime: new Date(0, 0, 0, 9, 20),
+        endTime: new Date(0, 0, 0, 17, 30)
+    }, {
+        id: _.uniqueId(),
+        name: 'Rat',
+        startTime: new Date(0, 0, 0, 8, 15),
+        endTime: new Date(0, 0, 0, 17, 15)
+    }];
+}
+
 const FleetView = {
     name: 'FleetView',
     data(){
         return {
-            couriers: [{
-                id: 'c0',
-                name: 'Badger',
-                startTime: new Date(0, 0, 0, 9),
-                endTime: new Date(0, 0, 0, 17)
-            }, {
-                id: 'c1',
-                name: 'Toad',
-                startTime: new Date(0, 0, 0, 8),
-                endTime: new Date(0, 0, 0, 22)
-            }],
+            couriers: demoCouriers(),
             selectedCourierIndex: -1,
         };
     },
@@ -37,43 +56,50 @@ const FleetView = {
         onSelectCourier(index){
             this.selectedCourierIndex = index;
         },
-        onUpdatedCourier(newVal){
+        onUpdateCourier(newVal){
             const index = _.findIndex(this.couriers, {id: newVal.id});
             this.couriers.splice(index, 1, newVal);
         },
         onAddCourier(ev){
-            this.couriers.push(defaultCourier());
+            this.couriers.splice(this.selectedCourierIndex, 0, defaultCourier());
         },
         onRemoveCourier(ev){
             const removeIndex = this.selectedCourierIndex;
-            this.selectedCourierIndex -= 1;
+            if(!window.confirm(`Remove ${this.couriers[removeIndex].name}?`)){
+                return;
+            }
             this.couriers.splice(removeIndex, 1);
+            if(this.selectedCourierIndex === this.couriers.length){
+                this.selectedCourierIndex = this.couriers.length - 1;
+            }
         }
     },
     render(){
         return (
             <div class="fleet">
                 <h2>Fleet</h2>
-                <CourierListView
-                    couriers={this.couriers}
-                    selectedCourierIndex={this.selectedCourierIndex}
-                    onSelectCourier={this.onSelectCourier}
-                />
+                <div class="courier-list-scroll">
+                    <CourierListView
+                        couriers={this.couriers}
+                        selectedCourierIndex={this.selectedCourierIndex}
+                        onSelectCourier={this.onSelectCourier}
+                    />
+                </div>
                 <input
                     type="button"
-                    value="+"
+                    value=" + "
                     onClick={this.onAddCourier}
                 />
                 <input
                     type="button"
-                    value="-"
+                    value=" - "
                     onClick={this.onRemoveCourier}
                     disabled={this.selectedCourierIndex < 0}
                 />
                 <h2>Courier</h2>
                 <CourierView
                     courier={this.selectedCourier}
-                    onUpdate={this.onUpdatedCourier}
+                    onUpdate={this.onUpdateCourier}
                 />
             </div>
         );
@@ -108,7 +134,10 @@ const CourierListView = {
                                    onChange={this.onChangeSelected}
                                 />
                                 <span>{courier.name}</span>
+                                &nbsp;
                                 <span>{courier.startTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</span>
+                                &nbsp;
+                                <span>{courier.endTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</span>
                             </label>
                         </li>
                     ))
@@ -125,8 +154,8 @@ const TimeInput = {
     },
     methods: {
         stringToTime(strTime){
-            const arr = strTime.split(':').map(function(item) { return parseInt(item) });
-            return new Date(0, 0, 0, arr[0], arr[1]);
+            const hhmm = strTime.split(':').map(item => parseInt(item, 10));
+            return new Date(0, 0, 0, hhmm[0], hhmm[1]);
         },
         timeToString(datetime){
             return datetime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
@@ -155,6 +184,11 @@ const CourierView = {
             editing: this.courier ? _.clone(this.courier) : defaultCourier()
         };
     },
+    computed: {
+        modified(){
+            return _.isEqual(this.courier, this.editing);
+        }
+    },
     watch: {
         courier(){
             this.reset();
@@ -172,37 +206,36 @@ const CourierView = {
         return (
             <fieldset class="courier" disabled={this.courier === null}>
                 <label>
-                    <span>name:&nbsp;</span>
-                    <input
-                        type="text"
-                        v-model={this.editing.name}
-                    />
+                    <span>Name:&nbsp;</span>
+                    <input type="text" v-model={this.editing.name} />
                 </label>
                 <br/>
                 <label>
-                    <span>start:&nbsp;</span>
+                    <span>Hours:&nbsp;</span>
                     <TimeInput v-model={this.editing.startTime} />
-                </label>
-                <br/>
-                <label>
-                    <span>end:&nbsp;</span>
                     <TimeInput v-model={this.editing.endTime}/>
                 </label>
-                <input type="button" value="Update" onClick={this.update}>Update</input>
-                <input type="button" value="Cancel" onClick={this.reset}>Cancel</input>
+                <div class="courier-buttons">
+                    <input type="button"
+                        value="Update"
+                        onClick={this.update}
+                        disabled={this.modified}
+                    >Update</input>
+                    &nbsp;
+                    <input
+                        type="button"
+                        value="Cancel"
+                        onClick={this.reset}
+                        disabled={this.modified}
+                    >Cancel</input>
+                </div>
             </fieldset>
         );
     }
-
 }
 
 
 const App = {
-    methods: {
-        onClickAwesome(ev){
-            alert(`Isn't it!`);
-        }
-    },
     name: 'App',
     render(){
         return (
