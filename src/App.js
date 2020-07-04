c053ca8a03a3b095f6bd84f4d7c408d8cbe4efce
Robert Byrne <robert@byrnemail.org>
2020-07-04 10:15:05 +0100

    Factor out list edit component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
 const FleetTable = require('./fleet-table.js');
+const ListEdit = require('./list-edit.js');
 
 function defaultCourier(){
     const id = _.uniqueId();
@@ -18,16 +19,24 @@ function demoCouriers(){
     ];
 }
 
-function scrollIfNeeded(element, container) {
-  if (element.offsetTop < container.scrollTop) {
-    container.scrollTop = element.offsetTop;
-  } else {
-    const offsetBottom = element.offsetTop + element.offsetHeight;
-    const scrollBottom = container.scrollTop + container.offsetHeight;
-    if (offsetBottom > scrollBottom) {
-      container.scrollTop = offsetBottom - container.offsetHeight;
+const CourierListItem = {
+    name: 'CourierListItem',
+    props: {
+        item: {type: Object, required: true},
+    },
+    methods: {
+        formatTime(datetime){
+            return datetime.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' });
+        }
+    },
+    render(){
+        return (
+            <span>
+                <span class="couriers-title">{this.item.name}</span>
+                <span class="couriers-deets">{this.formatTime(this.item.startTime)} - {this.formatTime(this.item.endTime)}</span>
+            </span>
+        )
     }
-  }
 }
 
 const FleetView = {
@@ -35,242 +44,26 @@ const FleetView = {
     props: {
         fleet: {type: Array, required: true}
     },
-    watch: {
-        fleet(){
-            this.reset();
-        },
-        selectedCourierIndex(newVal){
-            this._scrollListToIndex(newVal);
-        }
-    },
-    data(){
-        return {
-            couriers: _.clone(this.fleet),
-            selectedCourierIndex: this.fleet.length > 0 ? 0 : -1
-        };
-    },
-    computed: {
-        selectedCourier(){
-            const index = this.selectedCourierIndex;
-            return index > -1 ? this.couriers[index] : null;
-        },
-        modified(){
-            return _.isEqual(this.couriers, this.fleet);
-        },
-        courierStates(){
-            return _.map(this.couriers, courier => {
-                const fleetCourier = _.find(this.fleet, {id: courier.id});
-                if(!fleetCourier){
-                    return '+';
-                }
-                return _.isEqual(courier, fleetCourier) ? '' : '*';
-            });
-        }
-    },
     methods: {
-        _scrollListToIndex(index){
-            if(index < 0){
-                return;
-            }
-            const scroll = this.$refs.courierListScroll;
-            const el = this.$refs.courierList.$refs.courierRows[index];
-            el && scrollIfNeeded(el, scroll);
-        },
-        reset(){
-            this.couriers = _.clone(this.fleet);
-            this.selectedCourierIndex = this.fleet.length > 0 ? 0 : -1;
-        },
-        update(){
-            this.$emit('fleetChanged', this.couriers);
-        },
-        onSelectCourier(index){
-            this.selectedCourierIndex = index;
-        },
-        onUpdateCourier(newVal){
-            const index = _.findIndex(this.couriers, {id: newVal.id});
-            this.couriers.splice(index, 1, newVal);
-        },
-        onAddCourier(ev){
-            const pos = Math.max(this.selectedCourierIndex, 0);
-            this.couriers.splice(pos, 0, defaultCourier());
-            this.selectedCourierIndex = pos;
-            this._scrollListToIndex(pos);
-            this.$nextTick(() => this.$refs.courierView.focus());
-        },
-        onRemoveCourier(ev){
-            const removeIndex = this.selectedCourierIndex;
-            this.couriers.splice(removeIndex, 1);
-            if(this.selectedCourierIndex === this.couriers.length){
-                this.selectedCourierIndex = this.couriers.length - 1;
-                this._scrollListToIndex(this.selectedCourierIndex);
-            }
-        },
-        _move(offset){
-            const startPos = this.selectedCourierIndex;
-            this.couriers.splice(startPos + offset, 0, this.couriers.splice(startPos, 1)[0]);
-            this.selectedCourierIndex += offset;
-        },
-        onListKeyUp(ev){
-            if(ev.keyCode === 13){
-                this.$refs.courierView.focus();
-            }
-        },
-        onNext(){
-            this.selectedCourierIndex = (this.selectedCourierIndex + 1) % this.couriers.length;
-        },
-        onPrev(){
-            this.selectedCourierIndex = (this.selectedCourierIndex - 1) % this.couriers.length;
-        },
-        onMoveUp(ev){
-            this._move(-1);
-        },
-        onMoveDown(ev){
-            this._move(+1);
+        onFleetChanged(changed){
+            this.$emit('fleetChanged', changed);
         }
     },
     render(){
         return (
-            <div class="fleet">
-                <div class="fleet-header">
-                    <fieldset class="courier" disabled={this.selectedCourierIndex < 0}>
-                        <legend>Couriers</legend>
-                        <div class="actions">
-                            <input
-                                class="destructive-button"
-                                type="button"
-                                value={"Remove"}
-                                onClick={this.onRemoveCourier}
-                            />
-                        </div>
-                        <h2 class="courier-title">
-                            {this.selectedCourier ? this.selectedCourier.name : 'None'}
-                        </h2>
-                        <CourierView
-                            ref="courierView"
-                            courier={this.selectedCourier}
-                            onUpdate={this.onUpdateCourier}
-                            onRemove={this.onRemoveCourier}
-                        />
-                        <div class="navigation">
-                            <input
-                                type="button"
-                                onClick={this.onPrev}
-                                value="Prev"
-                                disabled={this.selectedCourierIndex <= 0}
-                            />
-                            &nbsp;
-                            <input
-                                type="button"
-                                onClick={this.onNext}
-                                value="Next"
-                                disabled={this.selectedCourierIndex >= this.couriers.length -1 }
-                            />
-                        </div>
-                    </fieldset>
-                    <div class="actions">
-                        <input type="button"
-                            class="submit-button"
-                            value="Update Fleet"
-                            onClick={this.update}
-                            disabled={this.modified}
-                        />
-                        &nbsp;
-                        <input
-                            type="button"
-                            class="destructive-button"
-                            value="Reset Fleet"
-                            onClick={this.reset}
-                            disabled={this.modified}
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="button"
-                            value={"New Courier"}
-                            onClick={this.onAddCourier}
-                        />
-                    </div>
-                </div>
-                <h2>Team</h2>
-                <div class="fleet-list-controls">
-                    <div class="fleet-updown">
-                        <input type="button"
-                            value={"\u2191"}
-                            onClick={this.onMoveUp}
-                            disabled={this.selectedCourierIndex < 1}
-                        />
-                        &nbsp;
-                        <input
-                            type="button"
-                            value={"\u2193"}
-                            onClick={this.onMoveDown}
-                            disabled={this.selectedCourierIndex < 0 || this.selectedCourierIndex >= this.couriers.length - 1}
-                        />
-                    </div>
-                </div>
-                <div ref="courierListScroll" class="courier-list-scroll">
-                    <CourierListView
-                        ref="courierList"
-                        nativeOnKeyup={this.onListKeyUp}
-                        couriers={this.couriers}
-                        courierStates={this.courierStates}
-                        selectedCourierIndex={this.selectedCourierIndex}
-                        onSelectCourier={this.onSelectCourier}
-                    />
-                </div>
-            </div>
+            <ListEdit
+                items={this.fleet}
+                onUpdateItems={this.onFleetChanged}
+                listItemComponent={CourierListItem}
+                formComponent={CourierView}
+                itemLegend="courier"
+                listLegend="team"
+                createDefaultItem={defaultCourier}
+                listClass="couriers"
+                listItemClass="couriers-row"
+            />
         );
     }
-};
-
-const CourierListView = {
-    name: 'CourierListView',
-    props: {
-        couriers: {type: Array, required: true},
-        courierStates: {type: Array, required: true},
-        selectedCourierIndex: {type: Number, required: true}
-    },
-    methods: {
-        onChangeSelected(ev){
-            this.$emit('selectCourier', Number(ev.target.value));
-        },
-        formatTime(datetime){
-            return datetime.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' });
-        }
-    },
-    render(){
-        return (
-            <ul ref="couriers" class="couriers">
-                {
-                    _.map(this.couriers, (courier, index) => (
-                        <li
-                            refInFor="true"
-                            ref="courierRows"
-                            class={ {
-                                selected: index === this.selectedCourierIndex,
-                                'couriers-row': true
-                            } }>
-                            <label>
-                                <input
-                                   class="hidden"
-                                   type="radio"
-                                   name="couriers"
-                                   checked={index === this.selectedCourierIndex}
-                                   value={index}
-                                   key={courier.id}
-                                   onChange={this.onChangeSelected}
-                                />
-                                <span class="couriers-title">{courier.name}</span>
-                                <span class="courier-state">&nbsp;{this.courierStates[index]}&nbsp;</span>
-                                <span class="couriers-deets">{this.formatTime(courier.startTime)} - {this.formatTime(courier.endTime)}</span>
-                            </label>
-                        </li>
-                    ))
-                }
-            </ul>
-        )
-    }
-
 }
 
 const TimeInput = {
@@ -304,16 +97,16 @@ const TimeInput = {
 const CourierView = {
     name: 'CourierView',
     props: {
-        courier: {type: Object, default: null}
+        original: {type: Object, default: null}
     },
     data(){
         return {
-            editing: this.courier ? _.clone(this.courier) : defaultCourier()
+            editing: this.original ? _.clone(this.original) : defaultCourier()
         };
     },
     watch: {
-        courier(){
-            this.editing = this.courier ? _.clone(this.courier) : defaultCourier()
+        original(){
+            this.editing = this.original ? _.clone(this.original) : defaultCourier()
         }
     },
     methods: {
@@ -327,7 +120,10 @@ const CourierView = {
     },
     render(){
         return (
-            <div>
+            <div class="courier-view">
+                <h2 class="courier-title">
+                    {this.original ? this.editing.name : 'None'}
+                </h2>
                 <label>
                     <span>Name:&nbsp;</span>
                     <input ref="nameInput" type="text" v-model={this.editing.name} onInput={this.update}/>
---
+++ src/list-edit.js
@@ -0,0 +1,272 @@
+const _ = require('lodash');
+require('./list-edit.css');
+
+function scrollIfNeeded(element, container) {
+  if (element.offsetTop < container.scrollTop) {
+    container.scrollTop = element.offsetTop;
+  } else {
+    const offsetBottom = element.offsetTop + element.offsetHeight;
+    const scrollBottom = container.scrollTop + container.offsetHeight;
+    if (offsetBottom > scrollBottom) {
+      container.scrollTop = offsetBottom - container.offsetHeight;
+    }
+  }
+}
+
+
+const ListEdit = {
+    name: 'ListEdit',
+    props: {
+        listLegend: {type: String, require: true},
+        itemLegend: {type: String, require: true},
+        items: {type: Array, required: true},
+        listItemComponent: {type: Object, required: true},
+        formComponent: {type: Object, required: true},
+        createDefaultItem: {type: Function, required: true},
+        listClass: {type: String, required: true},
+        listItemClass: {type: String, required: true}
+    },
+    watch: {
+        items(){
+            this.reset();
+        },
+        selectedItemIndex(newVal){
+            this._scrollListToIndex(newVal);
+        }
+    },
+    data(){
+        return {
+            pendingItems: _.clone(this.items),
+            selectedItemIndex: this.items.length > 0 ? 0 : -1
+        };
+    },
+    computed: {
+        selectedItem(){
+            const index = this.selectedItemIndex;
+            return index > -1 ? this.pendingItems[index] : null;
+        },
+        modified(){
+            return _.isEqual(this.pendingItems, this.items);
+        },
+        itemChangeStates(){
+            return _.map(this.pendingItems, pending => {
+                const original = _.find(this.items, {id: pending.id});
+                if(!original){
+                    return '+';
+                }
+                return _.isEqual(pending, original) ? '' : '*';
+            });
+        }
+    },
+    methods: {
+        _scrollListToIndex(index){
+            if(index < 0){
+                return;
+            }
+            const scroll = this.$refs.listScroll;
+            this.$refs.listView.scrollToIndex(index, scroll);
+        },
+        reset(){
+            this.pendingItems = _.clone(this.items);
+            this.selectedItemIndex = this.items.length > 0 ? 0 : -1;
+        },
+        update(){
+            this.$emit('updateItems', this.pendingItems);
+        },
+        onSelectItem(index){
+            this.selectedItemIndex = index;
+        },
+        onUpdateItem(newVal){
+            const index = _.findIndex(this.pendingItems, {id: newVal.id});
+            this.pendingItems.splice(index, 1, newVal);
+        },
+        onAddItem(ev){
+            const pos = Math.max(this.selectedItemIndex, 0);
+            this.pendingItems.splice(pos, 0, this.createDefaultItem());
+            this.selectedItemIndex = pos;
+            this._scrollListToIndex(pos);
+            this.$nextTick(() => this.$refs.formView.focus());
+        },
+        onRemoveItem(ev){
+            const removeIndex = this.selectedItemIndex;
+            this.pendingItems.splice(removeIndex, 1);
+            if(this.selectedItemIndex === this.pendingItems.length){
+                this.selectedItemIndex = this.pendingItems.length - 1;
+                this._scrollListToIndex(this.selectedItemIndex);
+            }
+        },
+        _move(offset){
+            const startPos = this.selectedItemIndex;
+            this.pendingItems.splice(startPos + offset, 0, this.pendingItems.splice(startPos, 1)[0]);
+            this.selectedItemIndex += offset;
+        },
+        onListKeyUp(ev){
+            if(ev.keyCode === 13){
+                this.$refs.formView.focus();
+            }
+        },
+        onNext(){
+            this.selectedItemIndex = (this.selectedItemIndex + 1) % this.pendingItems.length;
+        },
+        onPrev(){
+            this.selectedItemIndex = (this.selectedItemIndex - 1) % this.pendingItems.length;
+        },
+        onMoveUp(ev){
+            this._move(-1);
+        },
+        onMoveDown(ev){
+            this._move(+1);
+        }
+    },
+    render(){
+        const FormComponent = this.formComponent;
+        return (
+            <div class="list-edit">
+                <div class="list-header">
+                    <fieldset disabled={this.selectedItemIndex < 0}>
+                        <legend>{_.capitalize(this.itemLegend)}</legend>
+                        <div class="actions">
+                            <input
+                                class="destructive-button"
+                                type="button"
+                                value={"Remove"}
+                                onClick={this.onRemoveItem}
+                            />
+                        </div>
+                        <FormComponent
+                            ref="formView"
+                            original={this.selectedItem}
+                            onUpdate={this.onUpdateItem}
+                            onRemove={this.onRemoveItem}
+                        />
+                        <div class="navigation">
+                            <input
+                                type="button"
+                                onClick={this.onPrev}
+                                value="Prev"
+                                disabled={this.selectedItemIndex <= 0}
+                            />
+                            &nbsp;
+                            <input
+                                type="button"
+                                onClick={this.onNext}
+                                value="Next"
+                                disabled={this.selectedItemIndex >= this.pendingItems.length -1 }
+                            />
+                        </div>
+                    </fieldset>
+                    <div class="actions">
+                        <input type="button"
+                            class="submit-button"
+                            value={'Update ' +  this.listLegend}
+                            onClick={this.update}
+                            disabled={this.modified}
+                        />
+                        &nbsp;
+                        <input
+                            type="button"
+                            class="destructive-button"
+                            value={'Reset ' + this.listLegend}
+                            onClick={this.reset}
+                            disabled={this.modified}
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="button"
+                            value={'New ' + this.itemLegend}
+                            onClick={this.onAddItem}
+                        />
+                    </div>
+                </div>
+                <h2>Team</h2>
+                <div class="fleet-list-controls">
+                    <div class="fleet-updown">
+                        <input type="button"
+                            value={"\u2191"}
+                            onClick={this.onMoveUp}
+                            disabled={this.selectedItemIndex < 1}
+                        />
+                        &nbsp;
+                        <input
+                            type="button"
+                            value={"\u2193"}
+                            onClick={this.onMoveDown}
+                            disabled={this.selectedItemIndex < 0 || this.selectedItemIndex >= this.pendingItems.length - 1}
+                        />
+                    </div>
+                </div>
+                <div ref="listScroll" class="list-scroll">
+                    <ListView
+                        ref="listView"
+                        nativeOnKeyup={this.onListKeyUp}
+                        listItems={this.pendingItems}
+                        listItemStates={this.itemChangeStates}
+                        selectedIndex={this.selectedItemIndex}
+                        onListSelect={this.onSelectItem}
+                        listItemComponent={this.listItemComponent}
+                        listClass={this.listClass}
+                        listItemClass={this.listItemClass}
+                    />
+                </div>
+            </div>
+        );
+    }
+};
+
+const ListView = {
+    name: 'ListView',
+    props: {
+        listItemComponent: {type: Object, require: true},
+        listItems: {type: Array, required: true},
+        listItemStates: {type: Array, required: true},
+        selectedIndex: {type: Number, required: true},
+        listClass: {type: String, default: undefined},
+        listItemClass: {type: String, required: true}
+    },
+    methods: {
+        scrollToIndex(index, scroller){
+            const el = this.$refs.listItemRows[index];
+            el && scrollIfNeeded(el, scroller);
+        },
+        onChangeSelected(ev){
+            this.$emit('listSelect', Number(ev.target.value));
+        }
+    },
+    render(){
+        const ItemComponent = this.listItemComponent;
+        return (
+            <ul class={{ "list-view-list": true, [this.listClass]: this.listClass }} >
+                {
+                    _.map(this.listItems, (item, index) => (
+                        <li
+                            refInFor="true"
+                            ref="listItemRows"
+                            key={index}
+                            class={ {
+                                selected: index === this.selectedIndex,
+                                [this.listItemClass]: this.listItemClass
+                            } }>
+                            <label>
+                                <input
+                                   class="hidden"
+                                   type="radio"
+                                   name={this.listItemClass}
+                                   checked={index === this.selectedIndex}
+                                   value={index}
+                                   key={item.id}
+                                   onChange={this.onChangeSelected}
+                                />
+                                <span class="list-item-state">&nbsp;{this.listItemStates[index]}&nbsp;</span>
+                                <ItemComponent item={item} />
+                            </label>
+                        </li>
+                    ))
+                }
+            </ul>
+        )
+    }
+
+}
+
+module.exports = ListEdit;
